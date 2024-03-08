59c33289e7ac441aa27fffbb119cfe2373aef4d2
Sam <samuelrystone@gmail.com>
2024-03-08 01:01:43 -0500

    hero section v1

--- src/app/sections/projects/projects.component.ts
+++ src/app/sections/projects/projects.component.ts
@@ -1,10 +1,11 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { ProjectSummaryComponent } from '../../content/project-summary/project-summary.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [ProjectSummaryComponent],
+  imports: [CommonModule, ProjectSummaryComponent],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css',
   encapsulation: ViewEncapsulation.None
