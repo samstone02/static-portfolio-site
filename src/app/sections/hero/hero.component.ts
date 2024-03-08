96568178e650807cc2d7a3ee290ccc18157cca6a
Sam <samuelrystone@gmail.com>
2024-03-07 19:34:43 -0500

    theme overhaul. idk if I like it though

--- src/app/sections/hero/hero.component.ts
+++ src/app/sections/hero/hero.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-hero',
   standalone: true,
   imports: [],
   templateUrl: './hero.component.html',
-  styleUrl: './hero.component.css'
+  styleUrl: './hero.component.css',
+  encapsulation: ViewEncapsulation.None
 })
 export class HeroComponent {
 
